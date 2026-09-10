6a5090589bd81c88bfe753e56d24f7c46a3d4212
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2026-09-10 14:00:42 +0200

    making auth state equatable

--- Sources/Boomerang/Managers/AuthManager.swift
+++ Sources/Boomerang/Managers/AuthManager.swift
@@ -7,28 +7,6 @@
 
 import Foundation
 
-public enum AuthState: Sendable, Equatable {
-    // not logged in
-    case unauthenticated
-    // logged in
-    case authenticated(JWT?)
-    // token refresh in progress
-    case refreshing
-    
-    public static func == (_ lhs: AuthState, _ rhs: AuthState) -> Bool {
-        switch (lhs, rhs) {
-        case (.unauthenticated, .unauthenticated):
-            return true
-        case (.refreshing, .refreshing):
-            return true
-            case (.authenticated, .authenticated):
-            return true
-        default:
-            return false
-        }
-    }
-}
-
 actor AuthManager {
     
     private let urlSession: URLSession
